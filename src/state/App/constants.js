7182b2a8aa39b87e5e7ac1066f0f34d314931797
Amber Webb <amber.webb@teamsnap.com>
2018-04-22 17:46:28 -0500

    Remove api key

--- src/state/App/constants.js
+++ src/state/App/constants.js
@@ -5,4 +5,4 @@ export const APP_LOADING_START = 'APP_LOADING_START'
 // Can't hide the api key in a JS application...can't have mine...got to get your own
 // https://api.nasa.gov
 export const NASA_APOD_API = 'https://api.nasa.gov/planetary/apod?api_key='
-export const NASA_APOD_API_KEY = 'Zg45Pm3tJe5TohtxF6LprCoaC2gXCxIAcDFXH77c'
+export const NASA_APOD_API_KEY = ''
