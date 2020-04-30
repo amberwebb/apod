0c64479e2e04ee1a4dc3cc8cd1feedf75c4f9202
Amber Michelle Webb <amichelle.webb@gmail.com>
2020-04-30 18:48:12 -0500

    Remove api key

--- src/state/App/constants.js
+++ src/state/App/constants.js
@@ -5,4 +5,4 @@ export const APP_LOADING_START = 'APP_LOADING_START';
 // Can't hide the api key in a JS application...
 // https://api.nasa.gov
 export const NASA_APOD_API = 'https://api.nasa.gov/planetary/apod?api_key=';
-export const NASA_APOD_API_KEY = 'UISGy1CJE0ChYulRcR4WQq2cpjTA0w6AT3kaZFBE';
+export const NASA_APOD_API_KEY = '';
